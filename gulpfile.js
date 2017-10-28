dd4119f0b4baa9aeab481d9e1c0256ef43c7ccaf
rafal <ra88fi@o2.pl>
2017-10-28 18:22:35 +0200

    images minified

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,23 @@
+// Minify images
+
 var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
  
 gulp.task('compress-image', function() {
-    return gulp.src('./images/*.jpg')
-        .pipe(imagemin({ progressive: true, optimizationLevel: 7 }))
+    return gulp.src('./images/*.png')
+        .pipe(imagemin({ progressive: true, optimizationLevel: 10 }))
         .pipe(gulp.dest('./images_min'));
 });
+
+
+// Minify CSS
+
+/*let gulp = require('gulp');
+let cleanCSS = require('gulp-clean-css');
+
+gulp.task('minify-css', () => {
+  return gulp.src('./*.css')
+    .pipe(cleanCSS({compatibility: '*'}))
+    .pipe(gulp.dest('style'));
+});
+*/
